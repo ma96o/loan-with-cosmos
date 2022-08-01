cbe546c64ecab8fb51c10e409ba09339ae2ff8ce
masaaki1915 <masaaki1915@gmail.com>
2022-08-01 16:12:31 +0900

    fixed error when $ ignite chain serve

--- x/loan/types/expected_keepers.go
+++ x/loan/types/expected_keepers.go
@@ -3,5 +3,5 @@ package types
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
 type BankKeeper interface {
-	SendCoinsFromAccountToModule(ctx, sdk.Context, senderAddr, sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
