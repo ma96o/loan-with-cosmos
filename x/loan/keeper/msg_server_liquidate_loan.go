df8f0e17b42977b27c2d352c104ff7306aed7474
masaaki1915 <masaaki1915@gmail.com>
2022-08-02 13:47:05 +0900

    fixed error cuz wrong path

--- x/loan/keeper/msg_server_liquidate_loan.go
+++ x/loan/keeper/msg_server_liquidate_loan.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/username/loan/x/loan/types"
+	"loan-with-cosmos/x/loan/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
